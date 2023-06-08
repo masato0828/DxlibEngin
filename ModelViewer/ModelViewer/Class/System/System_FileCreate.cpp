5615839ff5f0771a45a1754697ef3d0ab3b52133
コノムラマサト <83739941+masato0828@users.noreply.github.com>
2023-06-08 16:25:36 +0900

    aaa

--- ModelViewer/ModelViewer/Class/System/System_FileCreate.cpp
+++ ModelViewer/ModelViewer/Class/System/System_FileCreate.cpp
@@ -2,35 +2,24 @@
 #include <iostream>
 #include <fstream>
 #include <filesystem>
+#include <Windows.h>
+#include <shobjidl.h>
+#include "../Common/Utility.h"
+
 #include "../../imGui/imgui.h"
 
 System_FileCreate::System_FileCreate()
 {
-    // ファイル名
     basePath_ = L"D:/UseEngin";
-    
-    std::ifstream file(basePath_);
-    if (file.is_open())
-    {
-        return;
-    }
-    else
-    {
-        userFileName_ = L"/BaseDirectory";
-
-        std::filesystem::create_directories(basePath_);
-        std::wstring systemPath = basePath_ +userFileName_;
-        std::filesystem::create_directory(systemPath);
-        
-        return;
-    }
+    fileDialog_open = false;
+    systemFullPath_ = basePath_;
 }
 
 System_FileCreate::~System_FileCreate()
 {
 }
 
-void System_FileCreate::CreateFile(const std::wstring& filePath)
+void System_FileCreate::Is_CreateFile(const std::wstring& filePath)
 {
     std::ofstream file(filePath);
     if (file)
@@ -47,11 +36,65 @@ void System_FileCreate::CreateFile(const std::wstring& filePath)
 
 bool System_FileCreate::IsMainFile()
 {
+    // フォルダ名
+
+
     ImGui::Begin(u8"データの読み込み");
 
+    if(fileDialog_open)
+    {
+        basePath_ = L"UseEngin";
+        // UseEnginフォルダがあるかどうか
+        std::ifstream file(basePath_);
+        if (file.is_open())
+        {
+            // UseEnginフォルダがある
+            //return true;
+        }
+        else
+        {
+            // UseEnginフォルダがない
+            // ファイルダイアログの表示
+            auto result = CreateDirectoryFromFileDialog();
 
+            if (result == "Null")
+            {
+                return false;
+            }
+           
+            // UseEnginフォルダの作成
+            result /= basePath_;
+            std::filesystem::create_directories(result);
+            systemFullPath_ = result;
+
+            //return true;
+        }
+        
+
+        fileDialog_open = false;
+    }
+    std::string utf8Str = Utility::WStringToUTF8(systemFullPath_);
+
+    ImGui::Text(u8"ファイルの参照");
+    char buffer[256]; // 適切なサイズに調整してください
+    strncpy_s(buffer, sizeof(buffer), utf8Str.c_str(), _TRUNCATE);
+    ImGui::InputText("##label", buffer, 1060);
+
+    std::string spath = buffer;
+    systemFullPath_ = Utility::StringToWideString(spath);
+    ImGui::SameLine();
+    if (ImGui::SmallButton(u8"参照"))
+    {
+        fileDialog_open = true;
+    }
 
     ImGui::End();
+
+    //再度開くときのため、
+    //systemFullPath_　を取得しておかなければならない
+
+
+    userFileName_ = L"/BaseDirectory";
 
 
     //// 既存のデータまたはデータの作成を行うかを聞く
@@ -75,23 +118,77 @@ void System_FileCreate::CreateFilesInDirectory(const std::wstring& directoryPath
 
     // MainFile
     std::wstring mainFilePath = directoryPath + L"/MainFile";
-    CreateFile(mainFilePath);
+    Is_CreateFile(mainFilePath);
 
     // System
     std::wstring systemPath = directoryPath + L"/System";
     std::filesystem::create_directory(systemPath);
     std::wstring systemFilePath = systemPath + L"/System.txt";
-    CreateFile(systemFilePath);
+    Is_CreateFile(systemFilePath);
 
     // Data
     std::wstring dataPath = directoryPath + L"/Data";
     std::filesystem::create_directory(dataPath);
     std::wstring dataFilePath = dataPath + L"/Data.json";
-    CreateFile(dataFilePath);
+    Is_CreateFile(dataFilePath);
 
     // Assets
     std::wstring assetsPath = directoryPath + L"/Assets";
     std::filesystem::create_directory(assetsPath);
     std::wstring assetsFilePath = assetsPath + L"/Assets.txt";
-    CreateFile(assetsFilePath);
+    Is_CreateFile(assetsFilePath);
 }
+
+std::filesystem::path System_FileCreate::CreateDirectoryFromFileDialog()
+{
+    // Common Item Dialogのインスタンスを作成
+    IFileDialog* pFileDialog;
+    HRESULT hr = CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&pFileDialog));
+    if (FAILED(hr))
+    {
+        // エラーメッセージを表示するか、エラーログに記録するなどの処理を行う
+        return "Null";
+    }
+
+    // ファイルダイアログをフォルダ選択モードに設定
+    DWORD options;
+    pFileDialog->GetOptions(&options);
+    pFileDialog->SetOptions(options | FOS_PICKFOLDERS);
+
+    // ファイルダイアログを表示
+    hr = pFileDialog->Show(NULL);
+    if (FAILED(hr))
+    {
+        // エラーメッセージを表示するか、エラーログに記録するなどの処理を行う
+        pFileDialog->Release();
+        return "Null";
+    }
+
+    // 選択されたフォルダのパスを取得
+    IShellItem* pShellItem;
+    hr = pFileDialog->GetResult(&pShellItem);
+    if (FAILED(hr))
+    {
+        // エラーメッセージを表示するか、エラーログに記録するなどの処理を行う
+        pFileDialog->Release();
+        return "Null";
+    }
+
+    PWSTR pszFolderPath;
+    hr = pShellItem->GetDisplayName(SIGDN_FILESYSPATH, &pszFolderPath);
+    if (FAILED(hr))
+    {
+        // エラーメッセージを表示するか、エラーログに記録するなどの処理を行う
+        pShellItem->Release();
+        pFileDialog->Release();
+        return "Null";
+    }
+
+    std::filesystem::path folderPath(pszFolderPath);
+    CoTaskMemFree(pszFolderPath);
+    pShellItem->Release();
+    pFileDialog->Release();
+
+    return folderPath;
+}
+
--- ModelViewer/ModelViewer/Class/System/System_FileCreate.h
+++ ModelViewer/ModelViewer/Class/System/System_FileCreate.h
@@ -1,16 +1,23 @@
 #pragma once
 #include <string>
+#include <filesystem>
 class System_FileCreate
 {
 public:
 	System_FileCreate();
 	~System_FileCreate();
 	void CreateFilesInDirectory(const std::wstring& filePath);
-	void CreateFile(const std::wstring& directoryPath);
+	void Is_CreateFile(const std::wstring& directoryPath);
 
 	bool IsMainFile();
 private:
+	std::filesystem::path CreateDirectoryFromFileDialog();
+
 	std::wstring basePath_;
 	std::wstring userFileName_;
+
+	bool fileDialog_open;
+
+	std::wstring systemFullPath_;
 };
 
